76807e066bc54a13f02614caabd165f3d2cd8702
geekduck <duck1218+github@gmail.com>
2017-08-10 19:46:23 +0900

    Update

--- src/main/java/jp/co/geekduck/camelsample/Application.java
+++ src/main/java/jp/co/geekduck/camelsample/Application.java
@@ -6,7 +6,14 @@ public class Application {
     public static void main(String[] args) throws Exception{
         System.out.println("start Application#main");
         Main main = new Main();
-        main.setFileApplicationContextUri("routesConfig/*.xml");
+        // String xmlRootDir = "routesConfig/**/";
+        // main.setFileApplicationContextUri(xmlRootDir + "/*.xml");
+        String xmlRootDir = "routesConfig/";
+        main.setFileApplicationContextUri(xmlRootDir + "/**/*.xml");
+        // main.setFileApplicationContextUri(xmlRootDir + "/camel-context.xml");
+        // main.setFileWatchDirectory(xmlRootDir + "/bar");
+        main.setFileWatchDirectory(xmlRootDir);
+        // main.setFileWatchDirectoryRecursively(true);
         main.run(args);
     }
 }
